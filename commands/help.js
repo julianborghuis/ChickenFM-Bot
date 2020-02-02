d5592c825453b1c68512060def8b81b480ccd46e
TheChicken14 <wissehes@gmail.com>
2020-02-02 14:20:04 +0100

    added queue to help

--- commands/help.js
+++ commands/help.js
@@ -6,16 +6,17 @@ exports.run = (client, message, args) => {
         .setTitle("Commands")
         .setColor(3447003)
         .setDescription(`
-        \`autojoin\`: Enables or disables the autojoin feature.
-        \`play\` or \`p\`: Plays ChickenFM in the users current channel.
-        \`stop\` or \`leave\`: Stops playing.
-        \`songs [page number]\`: Displays all available songs.
-        \`help\`: Displays this.
-        \`nowplaying\` or \`np\`: Displays the current playing track.
-        \`request\` or \`r\`: Request a song.
-        \`lyrics\` or \`l\`: Displays the lyrics of the current song.
+\`autojoin\`: Enables or disables the autojoin feature.
+\`play\` or \`p\`: Plays ChickenFM in the users current channel.
+\`stop\` or \`leave\`: Stops playing.
+\`songs [page number]\`: Displays all available songs.
+\`queue\`: Displays the queue
+\`help\`: Displays this.
+\`nowplaying\` or \`np\`: Displays the current playing track.
+\`request\` or \`r\`: Request a song.
+\`lyrics\` or \`l\`: Displays the lyrics of the current song.
 
-        Remember to use one of these prefixes:  \`${client.config.prefix.join("`, `")}\`
+Remember to use one of these prefixes:  \`${client.config.prefix.join("`, `")}\`
 `)
     message.channel.send(embed)
 }
