60f213d973b508484459c5f24b32ef88e11425f5
Wisse Hes <wissehes@Wisse-Catalina.local>
2020-02-08 02:05:47 +0100

    Updated to DiscordJS v12

--- commands/eval.js
+++ commands/eval.js
@@ -15,13 +15,13 @@ exports.run = (client, message, args) => {
         if(typeof evaled !== 'string')
             evaled = require('util').inspect(evaled);
 
-        const embed = new Discord.RichEmbed()
+        const embed = new Discord.MessageEmbed()
             .setColor('GREEN')
             .setTitle('Evaluation: Success')
             .setDescription(`\`\`\`xl\n${clean(evaled)}\n\`\`\``)
         message.channel.send(embed);
     } catch (err) {
-        const embed = new Discord.RichEmbed()
+        const embed = new Discord.MessageEmbed()
             .setColor('RED')
             .setTitle('Evaluation: Error')
             .setDescription(`\`\`\`xl\n${clean(err)}\n\`\`\``)
