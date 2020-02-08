157235e5f34c2996de300b45798343686300dddc
Wisse Hes <wissehes@Wisse-Catalina.local>
2020-02-08 02:06:08 +0100

    Updated to DiscordJS v12

--- commands/play.js
+++ commands/play.js
@@ -1,12 +1,10 @@
-exports.run = (client, message, args) => {
+exports.run = async (client, message, args) => {
 
   // Only try to join the sender's voice channel if they are in one themselves
-  if (message.member.voiceChannel) {
-    const connection = message.member.voiceChannel.join().then(connection => {
-    const dispatcher = connection.playStream('https://radio.chickenfm.com/radio/8000/radio.mp3?1571408930');
-    dispatcher.setVolume(0.5);
-    message.react("✅");
-    });
+  if (message.member.voice.channel) {
+    const connection = await message.member.voice.channel.join()
+    connection.play(client.broadcast)
+    message.react("✅")
   } else {
     message.reply('You need to join a voice channel first!');
   }
