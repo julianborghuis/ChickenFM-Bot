d626fd873eaaa5e25b32f06ec77ae9e3c3887e66
Wisse <wissehes@gmail.com>
2020-02-11 14:51:34 +0100

    updated play

--- commands/play.js
+++ commands/play.js
@@ -1,9 +1,14 @@
 exports.run = async (client, message, args) => {
 
   // Only try to join the sender's voice channel if they are in one themselves
-  if (message.member.voice.channel) {
+  if (client.voice.connections.get(message.guild.id)){
+    message.reply(`I'm already playing in \`${client.voice.connections.get(message.guild.id).channel.name}\``)
+  } else if (message.member.voice.channel) {
+    const broadcast  = client.getBroadcast()
     const connection = await message.member.voice.channel.join()
-    connection.play(client.broadcast)
+    broadcast.then(bc => {
+      connection.play(bc)
+    })
     message.react("✅")
   } else {
     message.reply('You need to join a voice channel first!');
