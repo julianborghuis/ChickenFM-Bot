d6823e56b727c2324f2833f7b55ea011fbe2156e
Wisse <wissehes@gmail.com>
2020-02-21 13:29:07 +0100

    Added mongodb functions

---
+++ mongo/models/guild.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const guildSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    guildID: String,
+    guildName: String,
+    ownerID: String,
+    ownerUsername: String,
+    prefix: Array,
+    autoJoinChannel: String
+});
+
+module.exports = mongoose.model('Guild', guildSchema);
