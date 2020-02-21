e721310f5b0256e34ca0d8bed4be5b2c226a76bc
Wisse <wissehes@gmail.com>
2020-02-21 18:53:40 +0100

    Update voiceStateUpdate.js

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -1,20 +1,31 @@
 module.exports = async (client, oldState, newState) => {
-    //if(newState)
-    try {
-        const guild = await client.getGuild(newState.guild)
-        if(guild && guild.autoJoinChannel){
-            const fetchedGuild = client.guilds.resolve(guild.guildID).channels
-            const channel = fetchedGuild.resolve(guild.autoJoinChannel)
-            if(channel.members.filter(member => !member.user.bot).size >= 1) {
-                client.getBroadcast("ChickenFM").then(async broadcast => {
-                    const connection = await channel.join()
-                    connection.play(broadcast.broadcast)
-                })
-            } else {
-                channel.leave()
-            }
+    if(newState.channel){
+        if(client.voice.connections.get(newState.guild.id)){
+            return
         }
-    } catch(e) {
-        console.error(e)
+
+        try {
+            const guild = await client.getGuild(newState.guild)
+            if(guild && guild.autoJoinChannel){
+                const fetchedGuild = client.guilds.resolve(guild.guildID).channels
+                const channel = fetchedGuild.resolve(guild.autoJoinChannel)
+                if(channel.members.filter(member => !member.user.bot).size >= 1) {
+                    client.getBroadcast("ChickenFM").then(async broadcast => {
+                        const connection = await channel.join()
+                        connection.play(broadcast.broadcast)
+                    })
+                }
+            }
+        } catch(e) {
+            console.error(e)
+        }
+    }
+
+    if(oldState.channel){
+        if(oldState.channel.members.filter(member => !member.user.bot).size === 0){
+            try {
+                oldState.channel.leave()
+            } catch(e) {}
+        }
     }
 }
